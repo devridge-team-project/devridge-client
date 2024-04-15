512c990bbd064d4733294c45cde81ff6f7fb68f6
jyj1111 <jyj202040@gmail.com>
2024-04-15 09:39:38 +0900

    [Feat] :footer link

---
+++ src/assets/data/footer.ts
@@ -0,0 +1,19 @@
+const footers = [
+  {
+    link: "https://github.com/",
+    src: "/images/icons/github.svg",
+    alt: "github",
+  },
+  {
+    link: "https://www.instagram.com/",
+    src: "/images/icons/instagram.png",
+    alt: "instagram",
+  },
+  {
+    link: "https://kr.linkedin.com/",
+    src: "/images/icons/linkedIn.png",
+    alt: "linkedIn",
+  },
+];
+
+export default footers;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export { default as auths } from "./data/auth";
+export { default as footers } from "./data/footer";
