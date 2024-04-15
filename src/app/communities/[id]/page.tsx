4d42719dd7a057964e1d7809e04c1426bff72c41
jyj1111 <jyj202040@gmail.com>
2024-04-15 23:38:29 +0900

    [Feat]: 커뮤니티 상세 페이지

--- src/app/communities/[id]/page.tsx
+++ src/app/communities/[id]/page.tsx
@@ -1,4 +1,12 @@
 import { Community } from "@/components";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { communityApi } from "@/connection";
 export default function CommunityPage() {
-  return <Community />;
+  const { id } = useParams();
+  const { data: post } = useQuery({
+    queryKey: ["getCommunity", id],
+    queryFn: () => communityApi.get(Number(id)),
+  });
+  return <Community post={post} />;
 }
--- src/components/communities/Community.tsx
+++ src/components/communities/Community.tsx
@@ -1,6 +1,58 @@
-import { useLocation, useParams } from "react-router-dom";
+import { PostCard, CommentCard } from "@/design";
+import { OnClick, Community } from "@/interface";
+import { cn } from "@/util";
+export default function CommunityById({
+  post,
+  createComment,
+}: {
+  post?: Community;
+  createComment?: OnClick;
+}) {
+  const container = {
+    displays: "flex flex-col gap-2",
+    paddings: "pb-2",
+    styles: "bg-white-off",
+  };
 
-export default function Community() {
-  const { id } = useParams();
-  return <div>{id}</div>;
+  const commentBox = {
+    container: {
+      displays: "flex flex-col items-center",
+      paddings: "px-8 pt-6.25",
+      sizes: "h-full",
+      styles: "bg-white",
+    },
+    body: {
+      displays: "flex flex-col gap-2.5",
+      sizes: " w-full max-w-120",
+    },
+    button: {
+      displays: "flex justify-center items-center",
+      sizes: "w-full h-12.5",
+      styles: "bg-bright-purple rounded-md ",
+      fonts: "text-white text-sm font-bold",
+    },
+  };
+  return (
+    <div>
+      <div className={cn(container)}>
+        <PostCard.Community
+          key={post?.id}
+          id={post?.id}
+          title={post?.title}
+          content={post?.content}
+          createdAt={post?.createdAt}
+          likes={post?.likes}
+          views={post?.views}
+          commentCount={post?.comments}
+          member={post?.member}
+        />
+      </div>
+      <div className={cn(commentBox.container)}>
+        <div className={cn(commentBox.body)}>
+          <div className="text-lg font-bold">답변하기</div>
+          <CommentCard.Create mutate={createComment} />
+        </div>
+      </div>
+    </div>
+  );
 }
