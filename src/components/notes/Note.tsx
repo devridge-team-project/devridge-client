1c24dae182753a8ac4d4bcc5174cd6f02e47d392
jyj1111 <jyj202040@gmail.com>
2024-04-16 23:59:10 +0900

    [Feat] 쪽지 api 연동

---
+++ src/components/notes/Note.tsx
@@ -0,0 +1,6 @@
+import { Note } from "@/interface";
+
+export default function NoteById({ posts }: { posts?: Note[] }) {
+  console.log(posts);
+  return <>쪽지</>;
+}
---
+++ src/components/notes/index.tsx
@@ -0,0 +1,2 @@
+export { default as Note } from "./Note";
+export { default as Notes } from "./Notes";
