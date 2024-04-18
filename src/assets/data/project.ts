3fd3afd2e09beaaf557982c03acee7b56f4c53e5
jyj1111 <jyj202040@gmail.com>
2024-04-19 00:06:35 +0900

    [Refactor]: 프로젝트 글 작성 select 컴포넌트 리팩토링

---
+++ src/assets/data/project.ts
@@ -0,0 +1,15 @@
+const roles = [
+  { id: "frontEnd", value: "프론트엔드" },
+  { id: "backEnd", value: "백엔드" },
+  { id: "design", value: "디자인" },
+  { id: "PM", value: "기획" },
+];
+const meeting = [
+  { id: "online", value: "온라인" },
+  { id: "offline", value: "오프라인" },
+];
+const project = {
+  roles,
+  meeting,
+};
+export default project;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as auths } from "./data/auth";
 export { default as footers } from "./data/footer";
+export { default as project } from "./data/project";
+export { default as studies } from "./data/study";
