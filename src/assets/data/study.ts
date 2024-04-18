9609ec9c914e2bec1450ed3a190138e9d32c6e77
jyj1111 <jyj202040@gmail.com>
2024-04-19 00:07:18 +0900

    [Refactor]: 스터디 글 작성 select 컴포넌트 리팩토링

---
+++ src/assets/data/study.ts
@@ -0,0 +1,8 @@
+const studies = [
+  { id: "CODING_TEST_STUDY", value: "코테 스터디" },
+  { id: "CS_STUDY", value: "CS 스터디" },
+  { id: "INTERVIEW_STUDY", value: "면접 스터디" },
+  { id: "ETC", value: "기타" },
+];
+
+export default studies;
