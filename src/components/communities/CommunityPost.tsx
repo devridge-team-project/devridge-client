8cf5096a0ebf0a981eece57c19fc44b751289a57
jyj1111 <jyj202040@gmail.com>
2024-04-19 00:03:27 +0900

    [Feat] 커뮤니티 글 작성 페이지 api 연동

---
+++ src/components/communities/CommunityPost.tsx
@@ -0,0 +1,34 @@
+import { OnClick, CommunityRequest } from "@/interface";
+import { useState } from "react";
+import { Button, Input, TextArea } from "@/design";
+
+export default function CommunityPost({
+  communityPost,
+}: {
+  communityPost: OnClick<CommunityRequest>;
+}) {
+  const title = useState<string>("");
+  const content = useState<string>("");
+
+  return (
+    <div>
+      <div>
+        <div className="text-xl font-bold">제목</div>
+        <Input
+          state={title}
+          placeholder="제목을 5자 이상 입력해주세요."
+          options={{ noOutline: true }}
+        />
+      </div>
+      <div>
+        <div className="text-xl font-bold">내용</div>
+        <TextArea state={content} placeholder="궁금한 것을 작성해보세요." />
+      </div>
+      <Button
+        title="작성하기"
+        onClick={() => communityPost({ title: title[0], content: content[0] })}
+        options={{ size: "full" }}
+      />
+    </div>
+  );
+}
--- src/components/communities/index.ts
+++ src/components/communities/index.ts
@@ -1,2 +1,3 @@
 export { default as Community } from "./Community";
 export { default as Communities } from "./Communities";
+export { default as CommunityPost } from "./CommunityPost";
