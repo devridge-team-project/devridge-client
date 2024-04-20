2a26ab5674ff5b78146f5a86d7b2f9059be68dfd
jyj1111 <jyj202040@gmail.com>
2024-04-20 16:01:55 +0900

    [UI] 스터디 작성 스타일링

--- src/components/studies/StudyPost.tsx
+++ src/components/studies/StudyPost.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Input, TextArea, Button, SelectButton } from "@/design";
+import { Tag, Input, TextArea, Button, SelectButton } from "@/design";
 import { OnClick, StudyRequest } from "@/interface";
 import { studies } from "@/assets";
 
@@ -20,40 +20,41 @@ export default function StudyPost({
   };
 
   return (
-    <div>
+    <div className="mx-8.75">
       <div>
-        <div className="text-xl font-bold mt-4 mb-2.5">제목</div>
+        <Tag title="제목" />
         <Input
           state={title}
           placeholder="제목을 5자 이상 입력해주세요."
           options={{ noOutline: true }}
         />
       </div>
-      <div>
-        <div className="text-xl font-bold mt-4 mb-2.5">위치</div>
+      <div className="mt-7.5">
+        <Tag title="위치" />
         <Input placeholder="위치를 입력해보세요" state={location} />
-        <div>
-          <div className="text-xl font-bold mt-4 mb-2.5">카테고리</div>
-          <div className="flex">
-            {studies.map(({ id, value }) => (
-              <SelectButton
-                key={id}
-                id={id}
-                type="radio"
-                name="category"
-                value={value}
-                checked={category === id}
-                onChange={onChange}
-                className=" w-[90px] h-7.5 border-2 border-gray-200"
-              />
-            ))}
-          </div>
+      </div>
+      <div className="mt-7.5">
+        <Tag title="카테고리" />
+        <div className="flex">
+          {studies.map(({ id, value }) => (
+            <SelectButton
+              key={id}
+              id={id}
+              type="radio"
+              name="category"
+              value={value}
+              checked={category === id}
+              onChange={onChange}
+              className=" w-[90px] h-7.5 border-2 border-gray-200"
+            />
+          ))}
         </div>
-        <div>
-          <div className="text-xl font-bold mt-4 mb-2.5">내용</div>
-          <TextArea state={content} placeholder="스터디 내용을 작성해보세요." />
-        </div>
-
+      </div>
+      <div className="mt-7.5">
+        <Tag title="내용" />
+        <TextArea state={content} placeholder="스터디 내용을 작성해보세요." />
+      </div>
+      <div className="mt-7.5">
         <Button
           title="작성하기"
           onClick={() =>
