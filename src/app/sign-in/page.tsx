44a70803da4e69311b8e59f0280d886e2de9df6b
jyj1111 <jyj202040@gmail.com>
2024-04-25 01:32:59 +0900

    [Feat] 로그인 성공시 홈으로

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,12 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
 import { SignIn } from "@/components";
 import { signApi } from "@/connection";
+import { useNavigate } from "react-router-dom";
+import { useSignUpStore } from "@/shared";
 
 export default function SignInPage() {
-  const { mutate } = useMutation({
+  const navigate = useNavigate();
+  const { setSignUpData } = useSignUpStore();
+  const { isSuccess, mutate } = useMutation({
     mutationKey: ["signIn"],
     mutationFn: signApi.in,
   });
+  if (isSuccess) {
+    alert("로그인 성공");
+    setSignUpData({ isSignedIn: true });
+    navigate("/");
+  }
 
   return <SignIn signIn={mutate} />;
 }
