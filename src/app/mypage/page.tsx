d4c36872a8c995c1254a403058f17970307bd5ce
jyj1111 <jyj202040@gmail.com>
2024-04-25 01:21:15 +0900

    [Feat]: useSignUpStore에 isSigned 변수 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,19 +2,26 @@ import { userApi, signApi } from "@/connection";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { MyAccount } from "@/components";
+import { useSignUpStore } from "@/shared";
 export default function MyPage() {
   const navigate = useNavigate();
+  const {
+    signUpData: { isSignedIn },
+    setSignUpData,
+  } = useSignUpStore();
   const { mutate, isSuccess } = useMutation({
     mutationKey: ["logout"],
     mutationFn: signApi.out,
   });
   if (isSuccess) {
     alert("로그아웃 됐습니다.");
+    setSignUpData({ isSignedIn: false });
     navigate("/");
   }
   const { data: user } = useQuery({
     queryKey: ["userDetails"],
     queryFn: userApi.get,
+    enabled: isSignedIn,
   });
 
   return <MyAccount logout={mutate} user={user} />;
