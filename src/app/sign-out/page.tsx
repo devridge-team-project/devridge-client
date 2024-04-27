968690be25ab69d0cf6677c682b4b84612291dc9
jyj1111 <jyj202040@gmail.com>
2024-04-27 20:31:36 +0900

    [Feat] 로그아웃 api 연동

--- src/app/sign-out/page.tsx
+++ src/app/sign-out/page.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
+import { useNavigation } from "@/hook";
 import { useSignUpStore } from "@/shared";
 import { signApi } from "@/connection";
 import { useMutation } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const SignOut = () => {
+  const navigate = useNavigation();
   const { setSignUpData } = useSignUpStore();
   const { mutate } = useMutation({
     mutationKey: ["logout"],
@@ -11,12 +14,21 @@ const SignOut = () => {
   });
 
   useEffect(() => {
-    (async () => {
-      setSignUpData({ isSignedIn: false });
-      mutate();
-      alert("로그아웃 되었습니다.");
-      window.location.href = "/";
-    })();
+    setSignUpData({
+      nickname: "",
+      occupation: "",
+      introduction: "",
+      imageUrl: null,
+      skillIds: [],
+      isSignedIn: false,
+    });
+    mutate();
+    Swal.fire({
+      title: "로그아웃 되었습니다.",
+      confirmButtonText: "확인",
+      confirmButtonColor: "#4F26F4",
+    });
+    navigate("/");
   }, []);
 
   return null;
